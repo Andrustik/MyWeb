64f9ca10ac5f433ff721f9c82b65e2bfb91f8e71
Andrustik <nice.iasin@gmail.com>
2025-12-07 11:02:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('moneyButton');
+
+btn.addEventListener('click', function () {
+    alert('Деньги сами не придут, но ты уже на пути к $1000 как веб-разраб 😎');
+});
